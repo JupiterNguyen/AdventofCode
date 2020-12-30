1ed3c1c90131995110fc257b5a3e8ecf88b75c2a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-30 05:00:05 -0600

    2015 Day03 Pt.1

---
+++ 2015/Day03/house.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# Desc: find two numbers that add up to 2020 and multiply them
+
+from itertools import product 
+
+text_file = open("problem.dat", "r")
+
+# Starting coordinates
+x = 0
+y = 0
+
+#empty list that will hold coordinates of houses
+my_list = []
+
+f = text_file.read()
+
+for element in range(0, len(f)):
+  if f[element] == "^":
+    x = x + 1
+  elif f[element] == "v":
+    x = x - 1
+  elif f[element] == ">":
+    y = y + 1
+  elif f[element] == "<":
+    y = y - 1
+
+  my_list.append("{},{}".format(x,y))
+
+# + 1 accounts for the beginning house
+print(len(set(my_list)) + 1)
+    
